66297ff5188e53f954483444b0621593ce84c502
Manuel Sandoval <manusandoval05@pop-os.localdomain>
2021-03-04 17:07:37 -0600

    Index page completed

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,15 +1,17 @@
 import React from "react"
 import Button from "./Button"
+import "./Item.css"
+
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 const Item = ({title, desc, descLink, backgroundImg, leftBtnTxt, leftBtnLink, rightBtnTxt, rightBtnLink, twoButtons, first}) =>{
     return(
         <div className="item" style={{
-
+            backgroundImage: `url(${backgroundImg})`
         }}>
             <div className="item_container">
                 <div className="item_text">
                     <p>{title}</p>
-                    <div className="item_textDec">
+                    <div className="item_textDesc">
                         <p>{desc}</p>
                     </div>
                 </div>
